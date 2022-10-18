b829480e86c593b3c9b949ff6a9100750a85a1fa
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-10-18 21:44:08 +0330

    change admin panel urlpattern

--- leave/settings.py
+++ leave/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['leaverequestapp-production.up.railway.app']
+ALLOWED_HOSTS = ['leaverequestapp-production.up.railway.app', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app/']
 
 # Application definition
--- leave/urls.py
+++ leave/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from decouple import config
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{config("ADMIN")}/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
 ]
